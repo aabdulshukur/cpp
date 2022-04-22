82cd30e5f4fc712569d42e369191e39f3bb2ad2f
aabdulshukur <aabdulshukur@gmail.com>
2022-04-22 12:50:47 -0500

    Update mystring.cc

--- cpp20/mystring.cc
+++ cpp20/mystring.cc
@@ -13,11 +13,27 @@ export bool is_palindrome(std::string& word) {
     int r = word.length()-1;
 
     while(l < r) {
-        if(word[l] != word[r]) return false;
-	l++; r--;
+        if(word[l++] != word[r--]) return false;
     }
 
     return true;
 }
+
+export void reverse(std::string& str) {
+    if(str.length() <=1 ) return;
+
+    int l=0;
+    int r=str.length()-1;
+
+    while(l<r) {
+        std::swap(str[l++], str[r--]);
+    }
+}
+
+
+// this method not accessible as it is not exported
+void not_accessible() {
+}
+
 
 } // namespace mystring
